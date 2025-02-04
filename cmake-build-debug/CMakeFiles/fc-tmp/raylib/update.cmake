98775010f92d1ec41f17bacd26e41bf5187c902e
Caedamon <156080309+Caedamon@users.noreply.github.com>
2025-02-04 11:57:58 +0100

    Add .gitignore to exclude build artifacts

--- cmake-build-debug/CMakeFiles/fc-tmp/raylib/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for raylib")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/Frozztie/Documents/00-CLion/Snake/cmake-build-debug/CMakeFiles/fc-tmp/raylib/raylib-gitupdate.cmake")
-
-endblock()
